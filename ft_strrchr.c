074a597844b3b8b0fdbc87f4772aa6e1b7c2aa8f
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2022-11-15 18:09:17 +0100

    fix strrchr not checking the first character

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -16,12 +16,11 @@ char	*ft_strrchr(const char *s, int c)
 {
 	size_t	len;
 
-	len = ft_strlen(s);
-	while (len)
+	len = ft_strlen(s) + 1;
+	while (len--)
 	{
 		if (*(s + len) == c)
 			return ((char *)(s + len));
-		len--;
 	}
 	return (0);
 }
