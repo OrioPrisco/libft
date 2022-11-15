1e063f4855717348e17a9262d8ecb7ca041d87e0
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2022-11-15 18:09:47 +0100

    strlcpy now uses existing functions

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -16,15 +16,11 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
 	size_t	i;
 
-	i = 0;
-	if (size != 0)
-	{
-		while ((i < size - 1) && src[i])
-		{
-			dest[i] = src[i];
-			i++;
-		}
-		dest[i] = '\0';
-	}
+	if (!size)
+		return (ft_strlen(src));
+	i = ft_strnlen(src, size - 1);
+	ft_memcpy(dest, src, i);
+	if (size)
+		dest[i] = 0;
 	return (ft_strlen(src));
 }
