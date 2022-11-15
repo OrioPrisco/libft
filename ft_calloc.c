f0cd8cc74d29a543667e64c5b084f48f8e41063c
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2022-11-15 18:09:13 +0100

    calloc strdup

---
+++ ft_calloc.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: OrioPrisco <47635210+OrioPrisco@users      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/07 18:36:58 by OrioPrisc         #+#    #+#             */
+/*   Updated: 2022/11/08 10:43:56 by OrioPrisc        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+//TODO : check for size_t max
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	size_t	total;
+
+	total = 0;
+	while (nmemb--)
+	{
+		total += size;
+		if (total < size)
+			return (0);
+	}
+	return (malloc(total));
+}
