a3ed93b1523695556a5eef926292ba118755d578
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2022-11-15 18:09:13 +0100

    memchr strchr strncmp strrchr (ty manon for confirming casting away const)

---
+++ ft_memchr.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: OrioPrisco <47635210+OrioPrisco@users      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/07 17:41:35 by OrioPrisc         #+#    #+#             */
+/*   Updated: 2022/11/07 18:01:55 by OrioPrisc        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char	*addr;
+
+	addr = (void *)s;
+	while (n--)
+	{
+		if (*addr++ == c)
+			return (addr);
+	}
+	return (0);
+}
