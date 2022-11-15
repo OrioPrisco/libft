cb3f2d0628cfb551301b08b42cfdecab756fbd5c
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2022-11-15 18:09:49 +0100

    one hecc of a memcmp (altho is it faster ?)

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -11,31 +11,57 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdint.h>
 
-int	ft_memcmp(const void *i1, const void *i2, size_t n)
+//a pretty standar memcmp
+static int	ft_memcmp_byte(const BYTE *s1, const BYTE *s2, size_t n)
 {
-	const unsigned int	*s1;
-	const unsigned int	*s2;
-	const unsigned char	*ss1;
-	const unsigned char	*ss2;
-
-	s1 = i1;
-	s2 = i2;
-	while (n && n >= sizeof(int))
+	while (n)
 	{
 		if (*s1 != *s2)
 			break ;
-		s1 += sizeof(int);
-		s2 += sizeof(int);
-		n -= sizeof(int);
-	}
-	ss1 = (const unsigned char *)s1;
-	ss2 = (const unsigned char *)s2;
-	while (n)
-	{
-		if (*ss1++ == *ss2++)
-			break ;
+		s1++;
+		s2++;
 		n--;
 	}
-	return (*ss1 - *ss2);
+	if (n)
+		return (*s1 - *s2);
+	return (0);
+}
+
+//do word comparisons. If a diference is found, use byte comparisons to get
+//the difference. once n is smalled than a word, do byte comparisons
+static int	ft_memcmp_word(const WORD_TYPE *s1, const WORD_TYPE *s2, size_t n)
+{
+	while (n >= sizeof(WORD_TYPE))
+	{
+		if (*s1 != *s2)
+			break ;
+		s1++;
+		s2++;
+		n -= sizeof(WORD_TYPE);
+	}
+	return (ft_memcmp_byte((const BYTE *)s1, (const BYTE *)s2, n));
+}
+
+//first try to align memory, then do full words comparisons
+int	ft_memcmp(const void *i1, const void *i2, size_t n)
+{
+	const BYTE	*s1;
+	const BYTE	*s2;
+	int			res;
+	size_t		shift;
+
+	shift = (intptr_t)i1 % sizeof(WORD_TYPE);
+	if (shift != ((intptr_t)i2 % sizeof(WORD_TYPE)))
+		return (ft_memcmp_byte(i1, i2, n));
+	res = ft_memcmp_byte(i1, i2, shift);
+	if (res)
+		return (res);
+	s1 = i1;
+	s2 = i2;
+	s1 += shift;
+	s2 += shift;
+	n -= shift;
+	return (ft_memcmp_word((const WORD_TYPE *)s1, (const WORD_TYPE *)s2, n));
 }
