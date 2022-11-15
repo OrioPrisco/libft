acb60818fa77093af4251c3c7fcf2a8926f082be
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2022-11-15 18:09:49 +0100

    WIP ft_memcmp using ints for max speed

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -14,20 +14,28 @@
 
 int	ft_memcmp(const void *i1, const void *i2, size_t n)
 {
-	const unsigned char	*s1;
-	const unsigned char	*s2;
+	const unsigned int	*s1;
+	const unsigned int	*s2;
+	const unsigned char	*ss1;
+	const unsigned char	*ss2;
 
 	s1 = i1;
 	s2 = i2;
-	while (n)
+	while (n && n >= sizeof(int))
 	{
 		if (*s1 != *s2)
 			break ;
-		s1++;
-		s2++;
+		s1 += sizeof(int);
+		s2 += sizeof(int);
+		n -= sizeof(int);
+	}
+	ss1 = (const unsigned char *)s1;
+	ss2 = (const unsigned char *)s2;
+	while (n)
+	{
+		if (*ss1++ == *ss2++)
+			break ;
 		n--;
 	}
-	if (!n)
-		return (0);
-	return (*s1 - *s2);
+	return (*ss1 - *ss2);
 }
