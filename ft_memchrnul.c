db5e776c37878c9bd037c67ab1c1d4c5ac421e52
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2022-11-15 18:09:46 +0100

    memchrnul

---
+++ ft_memchrnul.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchrnul.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: OrioPrisco <47635210+OrioPrisco@users      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/14 11:09:29 by OrioPrisc         #+#    #+#             */
+/*   Updated: 2022/11/14 11:12:17 by OrioPrisc        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_memchrnul(const void *s, unsigned char c, size_t n)
+{
+	unsigned char	*addr;
+
+	addr = s;
+	while (n--)
+	{
+		if (addr == c)
+			break ;
+		adrr++;
+	}
+	return (addr);
+}
--- libft.h
+++ libft.h
@@ -35,6 +35,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strchr(const char *s, char c);
 char	*ft_strrchr(const char *s, char c);
 void	*ft_memchr(const void *s, unsigned char c, size_t n);
+void	*ft_memchrnul(const void *s, unsigned char c, size_t n);
 int		ft_memcmp(const void *i1, const void *i2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_atoi(const char *str);
